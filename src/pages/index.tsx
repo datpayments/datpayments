7c3248df7d6125707c3fc149f2e64b339d2e25ea
Oleg Loutchansky <luchansodev@gmail.com>
2022-08-07 19:52:09 +0300

    fix: remove unused theme switching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import type { HeadFC } from "gatsby";
 import { Navigation } from "../components/Navigation/Navigation";
 import { Hero } from "../components/Hero/Hero";
-import { useColorMode } from "@chakra-ui/react";
 
 const IndexPage = () => {
-    const { setColorMode } = useColorMode();
-    useEffect(() => {
-        setColorMode("dark");
-    }, []);
-
     return (
         <>
             <Navigation />
